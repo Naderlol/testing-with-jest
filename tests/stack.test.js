5e0196840bfb1795a4d519eac7b1467c46de8acf
Naderr <naderking1000@gmail.com>
2024-05-08 17:19:05 +0200

    Lade till två enhetstester till stack.js

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,14 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+test('push() should add an element to the stack', () => {
+    stack.push(3);
+    expect(stack.peek()).toBe(3);
+});
+
+test('pop() should remove and return the top element from the stack', () => {
+    const poppedElement = stack.pop();
+    expect(poppedElement).toBe(2);
+    expect(stack.peek()).toBe(1);
+});
